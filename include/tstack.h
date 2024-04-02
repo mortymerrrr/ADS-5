a67c4929c3c4bfc5bc989c925d4feefa7a0dd159
mortymerrrr <160071899+mortymerrrr@users.noreply.github.com>
2024-04-02 16:22:24 +0300

    b58.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,37 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
+#include <string>
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+ private:
+    T arr[100];
+    int top;
+
+ public:
+    TStack(): top(-1) {}
+    void push(T value) {
+      if (!isFull())
+        arr[++top] = value;
+      else
+        throw std::string("Full");
+    }
+    T get() const {
+      return arr[top];
+    }
+    T pop() {
+      if (isEmpty())
+        throw std::string("Empty");
+      else
+        return arr[top--];
+    }
+    bool isEmpty() const {
+      return top == -1;
+    }
+    bool isFull() const {
+      return top == size - 1;
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
